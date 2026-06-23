7ff4d99342b7bd6a9bb35a3c443c5c990f4820ba
huen814 <ashtontehcreator@gmail.com>
2026-06-24 02:33:31 +0800

    Updated index to match with css

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Yosech Construction</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="newlogin.css">
 </head>
 <body>
 
